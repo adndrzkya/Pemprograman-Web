ade1ee29f7bd1a8327bff21a9faea38cb7cec635
adndrzkya <adindarazkya10@gmail.com>
2024-03-03 00:56:27 +0700

    first commit

---
+++ 211011400993_Pertemuan1.php
@@ -0,0 +1,20 @@
+<?php
+echo "<center><h1 style='color: red;'>Biodata</h1></center>";
+echo "<center><strong><span style='color: blue;'>Nama : </span></strong><span style='color: blue;'> <strong>ADINDA RAZKYA</strong></span></center>";
+echo "<center><span style='color: blue;'><strong>email :</strong></span> <span style='color: blue;'>razkyaa2@gmail.com</span></center>";
+echo "<center><span style='color: blue;'><strong>jurusan :</strong></span> <span style='color: blue;'>Teknik Informatika</span></center>";
+echo "<center><span style='color: blue;'><strong>Hoby :</strong></span> <span style='color: blue;'>Travelling</span></center>";
+?>
+<?php
+$message = "Sangat menyenangkan dan Menarik Sekali";
+echo "<!DOCTYPE html>";
+echo "<html>";
+echo "<head>";
+echo "<title>Pertemuan 1</title>";
+echo "</head>";
+echo "<body>";
+echo "<h1>Kesan Saya Belajar PHP</h1>";
+echo "<p>$message</p>";
+echo "</body>";
+echo "</html>";
+?>
